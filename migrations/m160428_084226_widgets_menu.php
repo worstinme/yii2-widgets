04519e1cd23b52dfd75dc8c484cde0bd8e07faa6
Evgeny Zakirov <box@flyleaf.su>
2016-10-29 02:12:55 +0800

    fix

--- migrations/m160428_084226_widgets_menu.php
+++ migrations/m160428_084226_widgets_menu.php
@@ -6,6 +6,12 @@ class m160428_084226_widgets_menu extends Migration
 {
     public function safeUp()
     {
+        $tableOptions = null;
+
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+        }
+
         $this->createTable('{{%widgets}}', [
             'id' => $this->primaryKey(),   
             'widget' => $this->string(),         
