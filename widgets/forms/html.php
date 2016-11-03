fd2acd88ef4c38fc6e91fbc2475abc3d1ad297f8
Evgeny Zakirov <box@flyleaf.su>
2016-11-03 10:27:00 +0800

    fix

--- widgets/forms/html.php
+++ widgets/forms/html.php
@@ -5,12 +5,17 @@ use worstinme\uikit\ActiveForm;
 
 $form = ActiveForm::begin(['layout'=>'horizontal','field_width'=>'full']); ?>
 
-<?=$form->field($model, "content")->widget(\worstinme\jodit\Editor::className(), [
-    'settings' => [
-        'buttons'=>[
-        	'source', '|', 'bold', 'italic', '|', 'ul', 'ol', '|', 'font', 'fontsize', 'brush', 'paragraph', '|','image', 'table', 'link', '|', 'left', 'center', 'right', 'justify', '|', 'undo', 'redo', '|', 'hr', 'eraser', 'fullsize'
+<?=\mihaildev\ckeditor\CKEditor::widget([
+    'model'=>$model,
+    'attribute'=>'content',
+    'editorOptions' => \mihaildev\elfinder\ElFinder::ckeditorOptions(['elfinder', 'path' => '/'],[
+        'preset' => 'standart',
+        'allowedContent' => true,
+        'height'=>'400px',
+        'toolbar' => [
+            ['Bold', 'Italic','Underline','-','NumberedList', 'BulletedList', '-', 'Link', 'Unlink','Styles','Font','FontSize','Format','TextColor','BGColor','-','Blockquote','CreateDiv','-','Image','Table','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','Outdent','Indent','-','RemoveFormat','Source','Maximize']
         ],
-    ],
-]);?>
+    ])
+])?>
 
 <?php ActiveForm::end(); ?>
