ffb5894e91e7a6c86fc87d14c43989a5f85b291c
Eugene Zakirov <worstinme@gmail.com>
2018-02-23 10:17:37 +0800

    uikit 3 assets

--- assets/AdminAsset.php
+++ assets/AdminAsset.php
@@ -9,18 +9,30 @@ class AdminAsset extends AssetBundle
     public $sourcePath = '@worstinme/widgets/assets';
 
     public $css = [
-        'css'=>'css/admin.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.35/css/uikit.min.css',
+        'css/admin.css',
     ];
 
     public $js = [
-        
+        'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.35/js/uikit.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.35/js/uikit-icons.min.js',
+        'https://use.fontawesome.com/e6846ff8c8.js',
     ];
 
     public $depends = [
-        'worstinme\uikit\UikitAsset',
+        'yii\web\YiiAsset',
     ];
 
-    public $publishOptions = [
-        'forceCopy'=> YII_ENV_DEV ? true : false,
-    ];
+    public function init()
+    {
+        if (YII_ENV_DEV) {
+
+            $this->publishOptions = [
+                'forceCopy'=> true,
+            ];
+
+        }
+
+        parent::init();
+    }
 }
