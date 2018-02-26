c8056e392f905cfef55791ff03efbb2e9b0291a2
Eugene Zakirov <worstinme@gmail.com>
2018-02-26 21:26:22 +0800

    moved base methods

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -2,35 +2,12 @@
 
 namespace worstinme\widgets\controllers;
 
-use Yii;
-use worstinme\widgets\models\Widgets;
 use worstinme\widgets\models\WidgetsSearch;
+use Yii;
 use yii\web\NotFoundHttpException;
 
-class DefaultController extends \yii\web\Controller
+class DefaultController extends Controller
 {
-    public function behaviors()
-    {
-        return [
-            'access' => [
-                'class' => \yii\filters\AccessControl::className(),
-                'rules' => [
-                    [
-                        'allow' => true,
-                        'roles' => Yii::$app->widgets->accessRoles,
-                    ],
-                ],
-            ],
-
-            'verbs' => [
-                'class' => \yii\filters\VerbFilter::className(),
-                'actions' => [
-                    'delete' => ['post','delete'],
-                ],
-            ],
-
-        ];
-    }
 
     public function actionIndex()
     {
@@ -77,7 +54,7 @@ class DefaultController extends \yii\web\Controller
 
                 Yii::$app->session->setFlash('success','Widget was updated');
 
-                return $this->redirect(['index']);
+                return $this->redirect(['update','id'=>$model->id]);
             }
             
         }
@@ -127,31 +104,5 @@ class DefaultController extends \yii\web\Controller
         return $this->redirect(['index']);
     }
 
-    protected function findModel($id)
-    {
-
-
-        if ($id === null && ($widget = $this->getWidget(Yii::$app->request->get('widget'))) !== null) {
-            $model = new Widgets(['state'=>1]);
-            $model->widgetModel = new $widget;
-            $model->widget = Yii::$app->request->get('widget');
-            return $model;
-        }
-        elseif (($model = Widgets::findOne($id)) !== null && ($widget = $this->getWidget($model->widget)) !== null) {
-            $model->widgetModel = new $widget;
-            $model->widgetModel->setAttributes(\yii\helpers\Json::decode($model->params));
-            return $model;
-        }
-        else {
-          //  throw new NotFoundHttpException('The requested page does not exist.');
-        }
-    }
-
-    protected function getWidget($widget) {
-
-        $widgetsModels = Yii::$app->widgets->widgetsModels;
-        return !empty($widgetsModels[$widget]) ? $widgetsModels[$widget] : null;
-
-    }
 
 }
