6ed319399198d928f2215aec2d240f36beb17ab9
Eugene Zakirov <worstinme@gmail.com>
2018-02-26 21:29:33 +0800

    uikit 3 buttons

--- views/default/_row-buttons.php
+++ views/default/_row-buttons.php
@@ -8,11 +8,11 @@ $last = isset($last)?$last:false;
 ?>
 
 <?php if ($key !== null && $key>0): ?>
-	<?= Html::a('Поднять вверх', $url = null, ['data-up'=>true]); ?> / 
+	<?= Html::a('Поднять вверх', $url = null, ['data-up'=>true],['class'=>'uk-button uk-button-primary']); ?> /
 <?php endif ?>
 
 <?php if (!$last): ?>
-	<?= Html::a('Вниз', $url = null,  ['data-down'=>true]); ?> / 
+	<?= Html::a('Вниз', $url = null,  ['data-down'=>true,'class'=>'uk-button uk-button-success']); ?> /
 <?php endif ?>
 
-<?= Html::a('Удалить', $url = null,  ['data-remove'=>true]); ?> 
+<?= Html::a('Удалить', $url = null,  ['data-remove'=>true,'class'=>'uk-button uk-button-danger']); ?>
